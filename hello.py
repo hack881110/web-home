be5bc074860251db64c270fe1a624165b9543355
wanghao <wanghao5@huaqin.com>
2019-11-06 15:58:13 +0800

    Add Form and session

--- hello.py
+++ hello.py
@@ -2,13 +2,30 @@ from flask import Flask
 from flask_script import Manager
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+#from flaskForm import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask import session, redirect, url_for, flash
 
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 bootstrap = Bootstrap(app)
 
+moment =  Moment(app)
+
 manager = Manager(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -20,9 +37,18 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name !=form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
